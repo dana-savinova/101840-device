a1c5dbd29cd5b3b88c63ac8f47484650a5484605
dana-savinova <savinovadana@ya.ru>
2018-12-14 11:36:13 +0300

    Добавлена анимация невалидной формы и сокращен код для закрытия по esc

--- js/script.js
+++ js/script.js
@@ -5,23 +5,35 @@ var mapBtn = document.querySelector(".contacts__map-link");
 var modalMap = document.querySelector(".modal-map");
 var closeMap = document.querySelector(".modal-map__close");
 
+var formFeedback = document.querySelector(".feedback__form");
+var userFeedback = document.querySelector("#user-name");
+var mailFeedback = document.querySelector("#user-mail");
+var textFeedback = document.querySelector("#feedback-text");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("userFeedback");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 contactsBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.add("modal-show");
+  if (storage) {
+      userFeedback.value = storage;
+      mailFeedback.focus();
+    } else {
+      userFeedback.focus();
+    }
 });
 
 closeFeedback.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (modalFeedback.classList.contains("modal-show")) {
-      modalFeedback.classList.remove("modal-show");
-    }
-  }
+  modalFeedback.classList.remove("modal-error");
 });
 
 mapBtn.addEventListener("click", function(evt) {
@@ -34,11 +46,30 @@ closeMap.addEventListener("click", function (evt) {
   modalMap.classList.remove("modal-show");
 })
 
-window.addEventListener("keydown", function (evt) {
+// закрытие по кнопке esc
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (modalMap.classList.contains("modal-show")) {
+    if (modalFeedback.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modalFeedback.classList.remove("modal-show");
+      modalFeedback.classList.remove("modal-error");
+    } else if (modalMap.classList.contains("modal-show")) {
+      evt.preventDefault();
       modalMap.classList.remove("modal-show");
     }
   }
+});
+
+formFeedback.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+  if (!userFeedback.value || !mailFeedback.value || !textFeedback.value ) {
+    evt.preventDefault();
+    modalFeedback.classList.remove("modal-error");
+    modalFeedback.offsetWidth = modalFeedback.offsetWidth;
+    modalFeedback.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+    localStorage.setItem("userFeedback", userFeedback.value);}
+  }
 })
+
