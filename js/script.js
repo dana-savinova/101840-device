a14491eda9a43f50a7f31b23c1421d99222c10cc
dana-savinova <savinovadana@ya.ru>
2018-12-14 10:48:10 +0300

    Измненено название переменной для закрытия формы обратной связи и добавлен показ карты

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
 var contactsBtn = document.querySelector(".contacts__button");
-var feedback = document.querySelector(".feedback");
-var close = document.querySelector(".close-button");
+var modalFeedback = document.querySelector(".feedback");
+var closeFeedback = document.querySelector(".feedback__close");
+var mapBtn = document.querySelector(".contacts__map-link");
+var modalMap = document.querySelector(".modal-map");
+var closeMap = document.querySelector(".modal-map__close");
 
 contactsBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  feedback.classList.add("modal-show");
+  modalFeedback.classList.add("modal-show");
 });
 
-close.addEventListener("click", function (evt) {
+closeFeedback.addEventListener("click", function (evt) {
   evt.preventDefault();
-  feedback.classList.remove("modal-show");
+  modalFeedback.classList.remove("modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (feedback.classList.contains("modal-show")) {
-      feedback.classList.remove("modal-show");
+    if (modalFeedback.classList.contains("modal-show")) {
+      modalFeedback.classList.remove("modal-show");
     }
   }
 });
+
+mapBtn.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalMap.classList.add("modal-show");
+})
+
+closeMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalMap.classList.remove("modal-show");
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modalMap.classList.contains("modal-show")) {
+      modalMap.classList.remove("modal-show");
+    }
+  }
+})
